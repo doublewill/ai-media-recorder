31261028c59e476303da48c2a86f3bd0cc205406
doublewill <xd8012@163.com>
2020-08-22 18:48:27 +0800

    优化静态引入

--- dist/ai-media-recorder.js
+++ dist/ai-media-recorder.js
@@ -1,95 +1,71 @@
-function initUserMedia() {
-  if (!navigator.mediaDevices) {
-    navigator.mediaDevices = {};
+(function (global, factory) {
+  typeof exports === "object" && typeof module !== "undefined"
+    ? (module.exports = factory())
+    : typeof define === "function" && define.amd
+    ? define(factory)
+    : (global.AIMediaRecorder = factory());
+})(this, function () {
+  "use strict";
+
+  //  https://developer.mozilla.org/zh-CN/docs/Web/API/MediaDevices/getUserMedia
+  function initUserMedia() {
+    if (!navigator.mediaDevices) {
+      navigator.mediaDevices = {};
+    }
+
+    if (!navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia = function (constraints) {
+        let getUserMedia =
+          navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+        if (!getUserMedia) {
+          return Promise.reject(
+            new Error("getUserMedia is not implemented in this browser")
+          );
+        }
+        return new Promise(function (resolve, reject) {
+          getUserMedia.call(navigator, constraints, resolve, reject);
+        });
+      };
+    }
   }
 
-  if (!navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia = function (constraints) {
-      let getUserMedia =
-        navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+  function checkTypeSupported() {
+    let types = [
+      "video/webm",
+      "audio/webm",
+      "video/webm;codecs=vp8",
+      "video/webm;codecs=daala",
+      "video/webm;codecs=h264",
+      "audio/webm;codecs=opus",
+      "video/mpeg",
+    ];
 
-      if (!getUserMedia) {
-        return Promise.reject(
-          new Error("getUserMedia is not implemented in this browser")
-        );
-      }
-      return new Promise(function (resolve, reject) {
-        getUserMedia.call(navigator, constraints, resolve, reject);
-      });
-    };
+    for (var i in types) {
+      console.log(
+        "Is " +
+          types[i] +
+          " supported? " +
+          (MediaRecorder.isTypeSupported(types[i]) ? "Maybe!" : "Nope :(")
+      );
+    }
   }
-}
 
-function checkTypeSupported() {
-  let types = [
-    "video/webm",
-    "audio/webm",
-    "video/webm;codecs=vp8",
-    "video/webm;codecs=daala",
-    "video/webm;codecs=h264",
-    "audio/webm;codecs=opus",
-    "video/mpeg",
-  ];
+  function AudioDataUtil(option) {
+    this.size = 0; //录音文件长度
+    this.buffer = []; //录音缓存
+    this.inputSampleRate = option.inputSampleRate || 16000; //输入采样率
+    this.inputSampleBits = option.inputSampleBits || 16; //输入采样数位 8, 16
+    this.outputSampleRate = option.outputSampleRate || 16000; //输出采样率
+    this.outputSampleBits = option.sampleBits || 16; //输出采样数位 8, 16
+    this.config = option;
 
-  for (var i in types) {
-    console.log(
-      "Is " +
-        types[i] +
-        " supported? " +
-        (MediaRecorder.isTypeSupported(types[i]) ? "Maybe!" : "Nope :(")
-    );
+    function closeContext() {
+      context.close(); //关闭AudioContext否则录音多次会报错。
+    }
   }
-}
 
-function AiMediaRecorder(config) {
-  // initUserMedia();
-  checkTypeSupported();
-  this.constraints = config.constraints;
-  this.audioContext = null;
-  this.recorder = null;
-  this.config = config;
-  this.init();
-}
-
-AiMediaRecorder.prototype.init = function () {
-  this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
-  // // https://developer.mozilla.org/zh-CN/docs/Web/API/AudioContext/createScriptProcessor
-  this.recorder = this.audioContext.createScriptProcessor(4096, 1, 1);
-  this.recorder.onaudioprocess = function (e) {
-    console.log(e.inputBuffer.getChannelData(0));
-  };
-  let videoEl  = this.config.video
-
-  navigator.mediaDevices
-        .getUserMedia(this.constraints)
-    .then((stream) => {
-      videoEl.srcObject = URL.createObjectURL(stream); // 此处的代码将会报错  解决的办法是将video的srcObject属性指向stream即可
-      videoEl.onloadedmetadata = (e) => {
-        videoEl.play();
-      }
-    })
-    .catch((error) => {
-      console.log(error.name + ": " + error.message);
-    });
-};
-
-let AudioDataUtil = function (option) {
-  let context = option.context;
-
-  let config = {
-    size: 0, //录音文件长度
-    buffer: [], //录音缓存
-    inputSampleRate: context.sampleRate, //输入采样率
-    inputSampleBits: 16, //输入采样数位 8, 16
-    outputSampleRate: option.sampleRate, //输出采样率
-    oututSampleBits: option.sampleBits, //输出采样数位 8, 16
-  };
-
-  function input(data) {
-    this.buffer.push(new Float32Array(data)); //Float32Array
-    this.size += data.length;
-  }
-  function getRawData() {
+  AudioDataUtil.prototype.getRawData = function () {
     //合并压缩
     var data = new Float32Array(this.size);
     var offset = 0;
@@ -109,10 +85,48 @@ let AudioDataUtil = function (option) {
       index++;
     }
     return result;
-  }
-  function getFullWavData() {
+  };
+
+  AudioDataUtil.prototype.getPureWavData = function (offset) {
+    var sampleBits = Math.min(this.inputSampleBits, this.outputSampleBits);
+    var bytes = this.getRawData();
+    var dataLength = bytes.length * (sampleBits / 8);
+    var buffer = new ArrayBuffer(dataLength);
+    var data = new DataView(buffer);
+    data = this.reshapeWavData(sampleBits, offset, bytes, data);
+    return new Blob([data], { type: "audio/wav" });
+  };
+  AudioDataUtil.prototype.reshapeWavData = function (
+    sampleBits,
+    offset,
+    iBytes,
+    oData
+  ) {
+    if (sampleBits === 8) {
+      for (var i = 0; i < iBytes.length; i++, offset++) {
+        var s = Math.max(-1, Math.min(1, iBytes[i]));
+        var val = s < 0 ? s * 0x8000 : s * 0x7fff;
+        val = parseInt(255 / (65535 / (val + 32768)));
+        oData.setInt8(offset, val, true);
+      }
+    } else {
+      for (var i = 0; i < iBytes.length; i++, offset += 2) {
+        var s = Math.max(-1, Math.min(1, iBytes[i]));
+        oData.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7fff, true);
+      }
+    }
+    return oData;
+  };
+
+  AudioDataUtil.prototype.input = function (data) {
+    this.buffer.push(new Float32Array(data)); //Float32Array
+    this.size += data.length;
+  };
+
+  AudioDataUtil.prototype.getFullWavData = function () {
+    let config = this.config;
     var sampleRate = Math.min(this.inputSampleRate, this.outputSampleRate);
-    var sampleBits = Math.min(this.inputSampleBits, this.oututSampleBits);
+    var sampleBits = Math.min(this.inputSampleBits, this.outputSampleBits);
     var bytes = this.getRawData();
     var dataLength = bytes.length * (sampleBits / 8);
     var buffer = new ArrayBuffer(44 + dataLength);
@@ -169,34 +183,111 @@ let AudioDataUtil = function (option) {
     // 写入采样数据
     data = this.reshapeWavData(sampleBits, offset, bytes, data);
     return new Blob([data], { type: "audio/wav" });
+  };
+
+  function H5MediaRecorder(config) {
+    initUserMedia();
+    checkTypeSupported();
+    this.constraints = config.constraints;
+    this.context = null;
+    this.recorder = null;
+    this.config = config;
+    this.recording = false;
+    this.videoChunks = [];
+    this.audioDataUtil = new AudioDataUtil({
+      channelCount: 1,
+    });
   }
-  function closeContext() {
-    context.close(); //关闭AudioContext否则录音多次会报错。
-  }
-  function getPureWavData(offset) {
-    var sampleBits = Math.min(this.inputSampleBits, this.oututSampleBits);
-    var bytes = this.getRawData();
-    var dataLength = bytes.length * (sampleBits / 8);
-    var buffer = new ArrayBuffer(dataLength);
-    var data = new DataView(buffer);
-    data = this.reshapeWavData(sampleBits, offset, bytes, data);
-    return new Blob([data], { type: "audio/wav" });
-  }
-  function reshapeWavData(sampleBits, offset, iBytes, oData) {
-    if (sampleBits === 8) {
-      for (var i = 0; i < iBytes.length; i++, offset++) {
-        var s = Math.max(-1, Math.min(1, iBytes[i]));
-        var val = s < 0 ? s * 0x8000 : s * 0x7fff;
-        val = parseInt(255 / (65535 / (val + 32768)));
-        oData.setInt8(offset, val, true);
-      }
-    } else {
-      for (var i = 0; i < iBytes.length; i++, offset += 2) {
-        var s = Math.max(-1, Math.min(1, iBytes[i]));
-        oData.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7fff, true);
-      }
+
+  H5MediaRecorder.prototype.start = function () {
+    if (this.context && this.context.state === "suspended") {
+      this.context.resume()
+      this.recording = true
+      return
     }
-    return oData;
-  }
-};
-window.AiMediaRecorder = AiMediaRecorder;
+
+    if (this.recording) {
+      return;
+    }
+
+    const success = (stream) => {
+      this.context = new (window.AudioContext || window.webkitAudioContext)();
+      // https://developer.mozilla.org/zh-CN/docs/Web/API/AudioContext/createScriptProcessor
+
+      let context = this.context;
+      this.recorder = context.createScriptProcessor(0, 1, 1);
+      let sampleRate = context.sampleRate;
+      this.audioDataUtil.inputSampleRate = sampleRate;
+      this.audioDataUtil.outputSampleRate = sampleRate;
+
+      this.recorder.onaudioprocess = (e) => {
+        let lData = e.inputBuffer.getChannelData(0);
+        if (!this.recording) {
+          return;
+        }
+        this.audioDataUtil.input(lData);
+      };
+      if (this.constraints.video) {
+        let mediaRecorder = new MediaRecorder(stream, {
+          audioBitsPerSecond: sampleRate || 16000,
+          videoBitsPerSecond: sampleRate || 256000,
+          mimeType: "video/webm",
+        });
+        mediaRecorder.start();
+
+        mediaRecorder.onstop = function (e) {
+          videoBlob = new Blob(this.videoChunks, { type: "video/mp4" });
+        };
+
+        mediaRecorder.ondataavailable = function (e) {
+          this.videoChunks.push(e.data);
+        };
+      }
+
+      const audioInput = context.createMediaStreamSource(stream);
+
+      audioInput.connect(this.recorder);
+      this.recorder.connect(context.destination);
+    };
+
+    navigator.mediaDevices
+      .getUserMedia(this.constraints)
+      .then((stream) => {
+        this.config.success(stream);
+        success(stream);
+      })
+      .catch((error) => {
+        console.log(error.name + ": " + error.message);
+      });
+    this.recording = true;
+  };
+
+  H5MediaRecorder.prototype.getBlob = function () {
+    return this.audioDataUtil.getFullWavData();
+  };
+
+  H5MediaRecorder.prototype.stop = function () {
+    if (!this.recording) {
+      return;
+    }
+    this.recording = false;
+  };
+
+  H5MediaRecorder.prototype.paused = function () {
+    if (this.context && this.context.state === "running") {
+      this.context.suspend();
+    }
+  };
+
+  H5MediaRecorder.prototype.download = function (fileName) {
+    let blob = this.getBlob();
+    let a = document.createElement("a");
+    a.href = window.URL.createObjectURL(blob);
+    a.download = (fileName || new Date().getTime()) + ".wav";
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(blob);
+    document.body.removeChild(a);
+  };
+  return H5MediaRecorder;
+});
